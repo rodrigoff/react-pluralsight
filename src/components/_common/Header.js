b348fbbb674d47dcf5119ea54bc9d0aefa20832a
Rodrigo F. Fernandes <rodrigoff@live.com>
2018-02-06 21:13:00 -0200

    Defaults to using 2 spaces for indentation

--- src/components/_common/Header.js
+++ src/components/_common/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <nav>
-            <NavLink to="/" activeClassName="active">Home</NavLink>
-            {" | "}
-            <NavLink to="/about" activeClassName="active">About</NavLink>
-        </nav>
-    );
+  return (
+    <nav>
+      <NavLink to="/" activeClassName="active">Home</NavLink>
+      {" | "}
+      <NavLink to="/courses" activeClassName="active">Courses</NavLink>
+      {" | "}
+      <NavLink to="/about" activeClassName="active">About</NavLink>
+    </nav>
+  );
 };
 
 export default Header;
